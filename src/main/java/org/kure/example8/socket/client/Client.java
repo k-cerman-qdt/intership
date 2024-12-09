92e1e5abf1c3127a112d4c727d437062440d5fb2
David Richter <d.richter@quadient.com>
2024-12-09 14:17:49 +0100

    Example 8: client server - TLS sockets extension Example 11: AES encryption

--- src/main/java/org/kure/example8/socket/client/Client.java
+++ src/main/java/org/kure/example8/socket/client/Client.java
@@ -1,19 +1,63 @@
 package org.kure.example8.socket.client;
 
-import org.kure.example8.socket.server.Server;
-
+import javax.net.ssl.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, KeyManagementException {
+        final SSLContext tls = SSLContext.getInstance("TLS");
+        tls.init(null, new TrustManager[]{
+                new X509ExtendedTrustManager() {
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] x509Certificates, String s, Socket socket) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] x509Certificates, String s, Socket socket) throws CertificateException {
+                    }
+
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }
+        }, new SecureRandom());
+        final SSLSocketFactory socketFactory = tls.getSocketFactory();
         try (
-                Socket localhost = new Socket("localhost", 10000);
+//                Socket localhost = new Socket("localhost", 10000);
+                Socket localhost = socketFactory.createSocket("localhost", 10000);
                 OutputStream os = localhost.getOutputStream();
                 final BufferedReader is = new BufferedReader(new InputStreamReader(localhost.getInputStream(), StandardCharsets.UTF_8))
         ) {
--- src/main/java/org/kure/example8/socket/server/Server.java
+++ src/main/java/org/kure/example8/socket/server/Server.java
@@ -1,13 +1,67 @@
 package org.kure.example8.socket.server;
 
-import java.io.IOException;
+import javax.net.ssl.*;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket ss = new ServerSocket(10000)) {
+    public static void main(String[] args) throws Exception {
+        final SSLContext tls = SSLContext.getInstance("TLS");
+        final KeyStore jks = KeyStore.getInstance("JKS");
+        try (InputStream inputStream = Files.newInputStream(Path.of("/tmp/enc.jks"))) {
+            jks.load(inputStream, "pass".toCharArray());
+        }
+        final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(jks, "pass".toCharArray());
+        tls.init(kmf.getKeyManagers(), new TrustManager[]{
+                new X509ExtendedTrustManager() {
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] x509Certificates, String s, Socket socket) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] x509Certificates, String s, Socket socket) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+                    }
+
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }
+        }, new SecureRandom());
+        final SSLServerSocketFactory serverSocketFactory = tls.getServerSocketFactory();
+//        try (ServerSocket ss = new ServerSocket(10000)) {
+        try (ServerSocket ss = serverSocketFactory.createServerSocket(10000)) {
             while (true) {
                 System.out.println("Waiting for client");
                 final Socket accept = ss.accept();
